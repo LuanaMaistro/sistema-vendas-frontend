d3e59fbf2d4ee88303cdb48ed28011968c71ef1f
LuanaMaistro <luanamaistro@gmail.com>
2025-12-29 14:54:40 -0300

    refactor: configurando rotas para a aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import CustomerCrudPage from './pages/customerCrud/CustomerCrudPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const appRouter = (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={ <CustomerCrudPage /> }/>
+
+      </Routes>
+    </BrowserRouter>
+  )
 
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {appRouter}
     </>
   )
 }
