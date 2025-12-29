4e81508b98097c9ec4fc999cb472eba6f298f1a1
LuanaMaistro <luanamaistro@gmail.com>
2025-12-29 18:17:59 -0300

    wip: página para cadastros de novos clientes
    Ainda sem os componentes do Ant Desing

--- src/pages/customerCrud/CustomerCrudPage.tsx
+++ src/pages/customerCrud/CustomerCrudPage.tsx
@@ -2,6 +2,17 @@ import styles from './CustomerCrudPage.module.css'
 
 export default function CustomerCrudPage() {
   return (
-    <div className={styles.customerCrudPage}>isso é um teste</div>
+    <div className={styles.customerCrudPage}>
+      <header>
+        <div>
+          <h1>Clientes</h1>
+          <p>Gerencie os clientes do sistema</p>
+        </div>
+
+        <div>
+          <button>+ Novo Cliente</button>
+        </div>
+      </header>
+    </div>
   )
 }
