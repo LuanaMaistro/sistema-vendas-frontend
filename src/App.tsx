ed692579a0813aacf657b07d1aae9861de6b54ad
LuanaMaistro <luanamaistro@gmail.com>
2025-12-30 20:20:51 -0300

    feat: configuração para theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CustomerCrudPage from './pages/customerCrud/CustomerCrudPage'
 import AppLayout from './pages/appLayout/AppLayout'
+import { ConfigProvider, type ThemeConfig } from 'antd'
 
 function App() {
 
@@ -20,9 +21,17 @@ function App() {
     </BrowserRouter>
   )
 
+  const theme = {
+    token: {
+      colorPrimary: "#A4B465"
+    }
+  } as ThemeConfig
+
   return (
     <>
-      {appRouter}
+      <ConfigProvider theme={theme}>
+        {appRouter}
+      </ConfigProvider>
     </>
   )
 }
