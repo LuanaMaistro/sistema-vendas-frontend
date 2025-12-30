6ac4632885f4a9925e12b05074261330b2863741
LuanaMaistro <luanamaistro@gmail.com>
2025-12-30 20:21:48 -0300

    refactor: configurações iniciais da tela de gestão de clientes

--- src/pages/customerCrud/CustomerCrudPage.tsx
+++ src/pages/customerCrud/CustomerCrudPage.tsx
@@ -1,18 +1,63 @@
+import { Button, Card, Flex, Form, Select } from 'antd'
 import styles from './CustomerCrudPage.module.css'
+import Title from 'antd/es/typography/Title'
+import Paragraph from 'antd/es/typography/Paragraph'
+import application from '../../infra/applicationInstance'
+import { useEffect, useState } from 'react'
+import { fold, type Customer } from '@dibimo/core-lib'
 
 export default function CustomerCrudPage() {
+  //TODO: converter para um Enum depois
+  const customerTypes = [
+    {
+      value: 0,
+      label: "Físico"
+    },
+    {
+      value: 1,
+      label: "Jurídico"
+    }
+  ]
+
+  const [clientes, setClientes] = useState<Array<Customer>>([])
+
+  const getCustomers = async () => {
+    const response = await application.ListCustomers.execute()
+    const customers = fold(response, () => [], (customers: Customer[]) => customers)
+
+    setClientes(customers)
+  }
+
+  useEffect(() => {
+    getCustomers()
+  })
+
   return (
     <div className={styles.customerCrudPage}>
-      <header>
-        <div>
-          <h1>Clientes</h1>
-          <p>Gerencie os clientes do sistema</p>
-        </div>
+      <header className={styles.crudPageHeader}>
+        <Flex vertical>
+          <Title level={2}>Clientes</Title>
+          <Paragraph>Gerencie os clientes do sistema</Paragraph>
+        </Flex>
 
         <div>
-          <button>+ Novo Cliente</button>
+          <Button variant="solid" color="primary">+ Novo Cliente</Button>
         </div>
       </header>
+
+      <Card>
+        <Form>
+          <Form.Item label="Tipo de cliente">
+            <Select options={customerTypes}/>
+          </Form.Item>
+        </Form>
+      </Card>
+
+      <div className="customers">
+        {clientes.map(c => (
+          <div>{c.name}</div>
+        ))}
+      </div>
     </div>
   )
 }
