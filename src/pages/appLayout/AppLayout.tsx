93dd412b472ae0006f9da646185cb0d722687bc7
LuanaMaistro <luanamaistro@gmail.com>
2025-12-30 11:41:38 -0300

    feat: posicionando elementos do layout da aplicação

--- src/pages/appLayout/AppLayout.tsx
+++ src/pages/appLayout/AppLayout.tsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
+import styles from "./AppLayout.module.css"
 
 export default function AppLayout() {
   return (
-    <main className="layoutContainer">
-
-      <div className="sidebar">
-      </div>
-
-      <div className="topbar"></div>
-
-      <div className="contentPage">
-        <Outlet />
+    <main className={styles.layoutContainer}>
+      <div className={styles.sidebar}></div>
+      <div className={styles.contentContainer}>
+        <div className={styles.topbar}></div>
+        <div className={styles.contentPage}></div>
       </div>
     </main>
   )
