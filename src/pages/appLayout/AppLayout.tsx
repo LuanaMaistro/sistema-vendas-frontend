69dbc8f18720b5dc17697f93eca56a6ea9961ebe
LuanaMaistro <luanamaistro@gmail.com>
2025-12-30 12:23:44 -0300

    feat: componente para topbar

--- src/pages/appLayout/AppLayout.tsx
+++ src/pages/appLayout/AppLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import styles from "./AppLayout.module.css"
 import AppLayoutSidebar from "./components/AppLayoutSidebar/AppLayoutSidebar";
+import AppLayoutTopbar from "./components/AppLayoutTopbar/AppLayoutTopbar";
 
 export default function AppLayout() {
   return (
     <main className={styles.layoutContainer}>
       <AppLayoutSidebar />
       <div className={styles.contentContainer}>
-        <div className={styles.topbar}></div>
+        <AppLayoutTopbar className={styles.topbar} />
         <div className={styles.contentPage}></div>
       </div>
     </main>
---
+++ src/pages/appLayout/components/AppLayoutTopbar/AppLayoutTopbar.tsx
@@ -0,0 +1,8 @@
+interface AppLayoutTopbarProps {
+  className: string
+}
+export default function AppLayoutTopbar({ className }: AppLayoutTopbarProps) {
+  return (
+    <div className={className}></div>
+  )
+}
