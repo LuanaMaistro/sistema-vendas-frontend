7f5f5eb377152dfebade5b1dee5bac92de834c30
LuanaMaistro <luanamaistro@gmail.com>
2025-12-30 23:00:00 -0300

    feat: criando função para mock de clientes fora da classe de service

--- src/infra/servicesImp/CustomerServiceImp.ts
+++ src/infra/servicesImp/CustomerServiceImp.ts
@@ -1,5 +1,26 @@
 import { CNPJ, type Customer, type CustomerService, type Result } from "@dibimo/core-lib";
 
+function createRandomCustomer() {
+
+  return {
+    id: '1',
+    name: 'Esse é um cliente de teste fixo apenas',
+    Cnpj: CNPJ.create("47.185.159/0001-91"),
+    CustomerContact: {
+      email: 'diogobissolimoreno@gmail.com',
+      phone: ''
+    }
+  }
+}
+const mockCustomers = [
+  createRandomCustomer(),
+  createRandomCustomer(),
+  createRandomCustomer(),
+  createRandomCustomer(),
+  createRandomCustomer(),
+  createRandomCustomer(),
+]
+
 export default class CustomerServiceImp implements CustomerService {
   Add(entity: Customer): Result {
     throw new Error("Method not implemented.");
