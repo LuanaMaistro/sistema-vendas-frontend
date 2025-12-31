7c1c0f874c7106465e9f0c8a2ab5ffff1979d120
LuanaMaistro <luanamaistro@gmail.com>
2025-12-30 23:18:23 -0300

    docs: comentário temporário

--- src/pages/customerCrud/CustomerCrudPage.tsx
+++ src/pages/customerCrud/CustomerCrudPage.tsx
@@ -76,6 +76,7 @@ export default function CustomerCrudPage() {
         </div>
       </header>
 
+      {/*esse é o card para filtros*/}
       <Card>
         <Form>
           <Form.Item label="Tipo de cliente">
