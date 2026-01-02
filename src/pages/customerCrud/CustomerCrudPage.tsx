dc6800e085ae8f90737bd2eb947645c816ef6986
LuanaMaistro <luanamaistro@gmail.com>
2026-01-01 22:27:08 -0300

    docs: TODO

--- src/pages/customerCrud/CustomerCrudPage.tsx
+++ src/pages/customerCrud/CustomerCrudPage.tsx
@@ -85,7 +85,7 @@ export default function CustomerCrudPage() {
         </Form>
       </Card>
 
-
+      {/*TODO: Existe um margin na páginação da table que faz a página scrollar, depois tem que ver como que faz para resolver corretamente*/}
       <Table<Customer>
         className={styles.customersTable}
         columns={columns}
