15021d403ed0fd58b35d74d67f29940cb674fe58
LuanaMaistro <luanamaistro@gmail.com>
2026-01-02 10:47:21 -0300

    feat: adicionando formulário para cliente físico

--- src/pages/customerCrud/components/CustomerForm/CustomerForm.tsx
+++ src/pages/customerCrud/components/CustomerForm/CustomerForm.tsx
@@ -6,33 +6,63 @@ interface CustomerFormProps {
 }
 
 export default function CustomerForm({ customerType }: CustomerFormProps) {
+
+  const legalPersonForm = (
+    <Row gutter={16}>
+      <Col span={12}>
+        <Form.Item label="Razão social">
+          <Input />
+        </Form.Item>
+      </Col>
+
+      <Col span={12}>
+        <Form.Item label="CNPJ">
+          <Input />
+        </Form.Item>
+      </Col>
+
+      <Col span={24}>
+        <Form.Item label="E-mail">
+          <Input />
+        </Form.Item>
+      </Col>
+    </Row>
+  )
+
+  const naturalPerson = (
+    <Row gutter={16}>
+      <Col span={12}>
+        <Form.Item label="Nome">
+          <Input />
+        </Form.Item>
+      </Col>
+
+      <Col span={12}>
+        <Form.Item label="Sobrenome">
+          <Input />
+        </Form.Item>
+      </Col>
+
+      <Col span={12}>
+        <Form.Item label="CPF">
+          <Input />
+        </Form.Item>
+      </Col>
+
+      <Col span={24}>
+        <Form.Item label="E-mail">
+          <Input />
+        </Form.Item>
+      </Col>
+    </Row>
+  )
   return (
 
     <div>
       <Form
         layout='vertical'
       >
-        <Row gutter={16}>
-
-          <Col span={12}>
-            <Form.Item label="Razão social">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={12}>
-            <Form.Item label="CNPJ">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={24}>
-            <Form.Item label="E-mail">
-              <Input />
-            </Form.Item>
-          </Col>
-
-        </Row>
+        { customerType == CustomerType.LEGAL_PERSON ? legalPersonForm : naturalPerson }
       </Form>
     </div>
   )
