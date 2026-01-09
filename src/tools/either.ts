35af4ad813ce2d1f041f2cecf340c863ae341e84
LuanaMaistro <luanamaistro@gmail.com>
2026-01-09 11:45:33 -0300

    feat: função para converter result em notification

--- src/tools/either.ts
+++ src/tools/either.ts
@@ -1,4 +1,36 @@
-import { fold, type Either } from "@dibimo/core-lib";
+import { fold, OperationError, type Either, type OperationResult } from "@dibimo/core-lib";
+import type { Notification } from "../hooks/notification/notification";
 
 export const eitherToBoolean = <L, R>(aEither: Either<L, R>) =>
   fold(aEither, () => false, () => true)
+
+export const operationResultToNotification = <T>(result: OperationResult<T>): Notification => {
+  return fold(result, errorToNotification, successToNotification)
+}
+
+const successToNotification = <T>(_: T): Notification => {
+  return {
+    title: 'Sucesso',
+    description: 'Operação realizada com sucesso!',
+    type: 'success'
+  }
+}
+
+
+const errorToNotification = (err: OperationError | Error): Notification => {
+  if(err instanceof OperationError) {
+    return {
+      title: `Error ao realizar operação (${err.code})`,
+      description: err.message,
+      type: 'error'
+    }
+  }
+
+  return {
+    title: 'Erro inesperado',
+    description: 'Ocorreu um erro inesperado ao realizar a operação',
+    type: 'error'
+  }
+
+}
+
