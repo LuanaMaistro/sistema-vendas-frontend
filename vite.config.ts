401d4f191f53af696c60cd0e0694716c295fed68
LuanaMaistro <luanamaistro@gmail.com>
2026-01-08 21:13:12 -0300

    chore: configurando vite e tsconfig para imports automaticos

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/,
+      ],
+
+      imports: [
+        'react'
+      ]
+    })
   ],
 })
