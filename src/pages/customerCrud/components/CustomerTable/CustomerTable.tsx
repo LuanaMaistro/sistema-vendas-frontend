bddeb038fb025adcff0e8f89b42a6b138b55c1d8
LuanaMaistro <luanamaistro@gmail.com>
2026-01-10 14:34:13 -0300

    feat: componente para table de clientes

---
+++ src/pages/customerCrud/components/CustomerTable/CustomerTable.tsx
@@ -0,0 +1,54 @@
+import type { Customer } from "@dibimo/core-lib"
+import { Space, Table, type TableColumnsType } from "antd"
+import styles from './CustomerTable.module.css'
+import { useCustomerCrudStore } from "../../CustomerCrudStore"
+
+interface CustomerTableProps {
+  editCustomer: (c: Customer) => void,
+  deleteCustomer: (c: Customer) => void,
+}
+
+export default function CustomerTable({ editCustomer, deleteCustomer }: CustomerTableProps) {
+
+  const { customers } = useCustomerCrudStore()
+
+  const columns: TableColumnsType<Customer> = [
+    {
+      title: 'Nome',
+      dataIndex: 'name'
+    },
+    {
+      title: 'E-mail',
+      dataIndex: ['CustomerContact', 'email'],
+    },
+    {
+      title: 'Cnpj',
+      dataIndex: ['Cnpj', 'Value']
+    },
+    {
+      title: 'Ações',
+      key: 'actions',
+      render: (_, customer) =>  (
+        <Space>
+          <a onClick={() => editCustomer(customer)}>Editar</a>
+          <a onClick={() => deleteCustomer(customer)}>Deletar</a>
+        </Space>
+      )
+    },
+  ]
+
+
+  return (
+    <div
+      className={styles.customersTable}
+    >
+      <Table<Customer>
+        scroll={{ y: 55 * 7 }}
+        columns={columns}
+        dataSource={customers}
+      />
+    </div>
+
+
+  )
+}
