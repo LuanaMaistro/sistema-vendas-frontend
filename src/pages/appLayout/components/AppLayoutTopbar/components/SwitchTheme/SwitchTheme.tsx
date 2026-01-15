b055e4a73d32aa45b03b6517a1b2d5d71a61d531
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 18:41:25 -0300

    refactor: implementando troca de no componente de switch

--- src/pages/appLayout/components/AppLayoutTopbar/components/SwitchTheme/SwitchTheme.tsx
+++ src/pages/appLayout/components/AppLayoutTopbar/components/SwitchTheme/SwitchTheme.tsx
@@ -1,10 +1,16 @@
 import { MoonOutlined, SunOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { useTheme } from "../../../../../../hooks/useTheme";
 
 export default function SwitchTheme() {
+  const { theme, toggleTheme } = useTheme();
   return (
     <div>
-      <SunOutlined />
-      <MoonOutlined />
+      <Button
+        icon={theme === 'light' ? <SunOutlined /> : <MoonOutlined />}
+        onClick={toggleTheme}
+      />
+
     </div>
   )
 }
