73033d83bcfe1544b0e69e5b498f605228839c44
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 14:18:51 -0300

    refactor: css para container de rotas da sidebar

--- src/pages/appLayout/components/AppLayoutSidebar/AppLayoutSidebar.tsx
+++ src/pages/appLayout/components/AppLayoutSidebar/AppLayoutSidebar.tsx
@@ -26,7 +26,7 @@ export default function AppLayoutSidebar() {
         {systemLogo}
       </div>
 
-      <nav>
+      <nav className={styles.routesContainer}>
         {routesConfigs.map(toLink)}
       </nav>
 
