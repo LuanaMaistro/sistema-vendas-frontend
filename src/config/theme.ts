367f376ae2cf7e24bcdf2938f02a119de44301bb
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 19:16:14 -0300

    fix: bug no tema dark

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -27,7 +27,19 @@ export const lightTheme: ThemeConfig = {
 export const darkTheme: ThemeConfig = {
   algorithm: theme.darkAlgorithm,
   token: {
-    ...lightTheme.token,
     colorPrimary: "#36CFC9",
+    borderRadius: 8,
+    borderRadiusLG: 12,
+    borderRadiusSM: 4,
+    boxShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.03), 0 1px 6px -1px rgba(0, 0, 0, 0.02)",
+    boxShadowSecondary: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+    fontSize: 14,
+    fontFamily: "system-ui, Avenir, Helvetica, Arial, sans-serif",
+    padding: 16,
+    paddingLG: 24,
+    paddingSM: 12,
+    margin: 16,
+    marginLG: 24,
+
   }
 };
