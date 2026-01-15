ecf20fa03f0958156ff46793c50964ef50cee082
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 15:40:45 -0300

    feat: componente para troca de tema

---
+++ src/pages/appLayout/components/AppLayoutTopbar/components/SwitchTheme.tsx
@@ -0,0 +1,10 @@
+import { MoonOutlined, SunOutlined } from "@ant-design/icons";
+
+export default function SwitchTheme() {
+  return (
+    <div>
+      <SunOutlined />
+      <MoonOutlined />
+    </div>
+  )
+}
