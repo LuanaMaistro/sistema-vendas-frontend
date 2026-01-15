44dbc0df891d2d68ed87351734af074b5fdebc54
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 12:31:52 -0300

    feat: espaço para logo do sistema na sidebar

--- src/pages/appLayout/components/AppLayoutSidebar/AppLayoutSidebar.tsx
+++ src/pages/appLayout/components/AppLayoutSidebar/AppLayoutSidebar.tsx
@@ -1,7 +1,21 @@
 import styles from './AppLayoutSidebar.module.css'
 
 export default function AppLayoutSidebar() {
+
+  const systemLogo = (
+    <div className={ styles.systemLogo }>
+      S
+    </div>
+  )
+
   return (
-    <div className={styles.sidebar}></div>
+    <div>
+
+      <div className={styles.systemLogoContainer}>
+        {systemLogo}
+      </div>
+
+
+    </div>
   )
 }
