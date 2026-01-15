08ec8763a7b29271f7a491ff930b4621f868fbc7
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 17:17:16 -0300

    feat: posicionando elementos inciais na topbar

--- src/pages/appLayout/components/AppLayoutTopbar/AppLayoutTopbar.tsx
+++ src/pages/appLayout/components/AppLayoutTopbar/AppLayoutTopbar.tsx
@@ -1,8 +1,14 @@
+import { BellOutlined, UserOutlined } from '@ant-design/icons'
 import styles from './AppLayoutTopbar.module.css'
+import { Avatar } from 'antd'
+import SwitchTheme from './components/SwitchTheme'
 
 export default function AppLayoutTopbar() {
   return (
     <div className={styles.topbarContainer}>
+      <BellOutlined />
+      <SwitchTheme />
+      <Avatar icon={<UserOutlined />} />
     </div>
   )
 }
