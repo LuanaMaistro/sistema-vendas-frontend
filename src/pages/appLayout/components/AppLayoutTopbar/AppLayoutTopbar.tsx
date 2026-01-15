62aab2d2a237699055aec59a20ad1e57af35f987
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 18:12:05 -0300

    refactor: criando pasta para componente de troca de tema

--- src/pages/appLayout/components/AppLayoutTopbar/AppLayoutTopbar.tsx
+++ src/pages/appLayout/components/AppLayoutTopbar/AppLayoutTopbar.tsx
@@ -1,7 +1,7 @@
 import { UserOutlined } from '@ant-design/icons'
 import styles from './AppLayoutTopbar.module.css'
 import { Avatar } from 'antd'
-import SwitchTheme from './components/SwitchTheme'
+import SwitchTheme from './components/SwitchTheme/SwitchTheme'
 import NotificationsBell from './components/NotificationsBell/NotificationsBell'
 
 export default function AppLayoutTopbar() {
