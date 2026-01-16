6068a4720876e2ae76d97e06690c08e078c5ba32
LuanaMaistro <luanamaistro@gmail.com>
2026-01-16 10:26:01 -0300

    feat: implementando métodos para mock da tela de produtos

--- src/infra/servicesImp/ProductServiceImp.ts
+++ src/infra/servicesImp/ProductServiceImp.ts
@@ -1,20 +1,79 @@
-import type { Product, ProductService, Result } from "@dibimo/core-lib";
+import { fakerPT_BR as faker } from '@faker-js/faker'
+import { Price, Quantity, type Product, type ProductService, type Result } from "@dibimo/core-lib";
+
+faker.seed(123)
+
+function createRandomProduct(): Product {
+  return {
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    code: faker.string.alphanumeric(10).toUpperCase(),
+    price: Price.create(Number(faker.commerce.price())),
+    quantity: Quantity.create(faker.number.int({ min: 0, max: 1000 }))
+  }
+}
+
+let mockProducts = Array.from({ length: 15 }, createRandomProduct)
 
 export default class ProductServiceImp implements ProductService {
+
   Add(entity: Product): Result {
-    throw new Error("Method not implemented.");
-  }
-  Remove(id: string): Result {
-    throw new Error("Method not implemented.");
-  }
-  Update(entity: Product): Result {
-    throw new Error("Method not implemented.");
-  }
-  List(): Result<Product[]> {
-    throw new Error("Method not implemented.");
-  }
-  GetById(id: string): Result<Product> {
-    throw new Error("Method not implemented.");
+    mockProducts.unshift(entity)
+
+    return {
+      code: 200,
+      success: true,
+    }
   }
 
+  Remove(id: string): Result {
+    const productIndex = this.getProductIndex(id)
+    delete mockProducts[productIndex]
+    mockProducts = mockProducts.filter(Boolean)
+    return {
+      code: 200,
+      success: true,
+    }
+  }
+
+  Update(entity: Product): Result {
+    const productIndex = this.getProductIndex(entity.id!)
+    const productUpdated = { ...mockProducts[productIndex] }
+    productUpdated.name = entity.name
+    productUpdated.description = entity.description
+    productUpdated.code = entity.code
+    productUpdated.price = entity.price
+    productUpdated.quantity = entity.quantity
+
+    mockProducts[productIndex] = productUpdated
+    return {
+      code: 200,
+      success: true,
+    }
+  }
+
+  List(): Result<Product[]> {
+    return {
+      data: [
+        ...mockProducts
+      ],
+      success: true,
+      code: 200,
+    }
+  }
+
+  GetById(id: string): Result<Product> {
+    return {
+      success: true,
+      code: 200,
+      data: mockProducts[this.getProductIndex(id)]
+    }
+  }
+
+  private getProductIndex(productId: string) {
+    const result = mockProducts.findIndex((p) => p.id! == productId)
+    if(result < 0) throw 'produto não encontrado'
+    return result
+  }
 }
