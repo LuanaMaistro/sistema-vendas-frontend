34e4dcd354c8ea2e961672c97f6bd0e120c331eb
LuanaMaistro <luanamaistro@gmail.com>
2026-01-16 10:42:21 -0300

    feat: formulário para produto

---
+++ src/pages/productCrud/components/ProductForm/ProductForm.tsx
@@ -0,0 +1,61 @@
+import { Col, Form, Input, InputNumber, Row, type FormInstance, type FormProps } from "antd";
+import type ProductFormFields from "../../types/ProductFormFields";
+
+interface ProductFormProps {
+  form: FormInstance<ProductFormFields>
+  onFinish: FormProps['onFinish']
+}
+
+export default function ProductForm({ form, onFinish }: ProductFormProps) {
+
+  return (
+    <div>
+      <Form
+        form={form}
+        layout='vertical'
+        onFinish={onFinish}
+      >
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item name="name" label="Nome do produto">
+              <Input />
+            </Form.Item>
+          </Col>
+
+          <Col span={12}>
+            <Form.Item name="code" label="Código">
+              <Input />
+            </Form.Item>
+          </Col>
+
+          <Col span={24}>
+            <Form.Item name="description" label="Descrição">
+              <Input.TextArea rows={4} />
+            </Form.Item>
+          </Col>
+
+          <Col span={12}>
+            <Form.Item name="price" label="Preço">
+              <InputNumber
+                prefix="R$"
+                min={0}
+                step={0.01}
+                style={{ width: '100%' }}
+              />
+            </Form.Item>
+          </Col>
+
+          <Col span={12}>
+            <Form.Item name="quantity" label="Quantidade">
+              <InputNumber
+                min={0}
+                step={1}
+                style={{ width: '100%' }}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  )
+}
