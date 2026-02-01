e8b96ce402a98b973ad6f2c39eec3647f5718496
LuanaMaistro <luanamaistro@gmail.com>
2026-02-01 18:06:29 -0300

    feat: injetando implementação de services para vendas

--- src/infra/servicesImp/serviceFactiory.ts
+++ src/infra/servicesImp/serviceFactiory.ts
@@ -1,10 +1,12 @@
 import type { ServicesFactories } from "@dibimo/core-lib";
 import CustomerServiceImp from "./CustomerServiceImp";
 import ProductServiceImp from "./ProductServiceImp";
+import SaleServiceImp from "./SaleServiceImp";
 
 export const serviceFactory = {
   CustomerService: () => new CustomerServiceImp(),
-  ProductService: () => new ProductServiceImp()
+  ProductService: () => new ProductServiceImp(),
+  SaleService: () => new SaleServiceImp()
 
 } as ServicesFactories
 
