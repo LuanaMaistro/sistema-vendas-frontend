88dee954e52e6515176dd28816e3d189b01ea517
LuanaMaistro <luanamaistro@gmail.com>
2026-04-04 11:12:29 -0300

    feat: componente para seletor de top para os gráficos

---
+++ src/pages/dashboard/components/TopSelector/TopSelector.tsx
@@ -0,0 +1,26 @@
+import { Select } from 'antd'
+import type { TopOption } from '../../hooks/useRelatorios'
+
+interface TopSelectorProps {
+  value: TopOption
+  onChange: (value: TopOption) => void
+}
+
+const TOP_OPTIONS = [
+  { label: 'Top 5', value: 5 },
+  { label: 'Top 10', value: 10 },
+  { label: 'Top 15', value: 15 },
+  { label: 'Top 30', value: 30 },
+]
+
+export default function TopSelector({ value, onChange }: TopSelectorProps) {
+  return (
+    <Select
+      value={value}
+      options={TOP_OPTIONS}
+      onChange={(val: TopOption) => onChange(val)}
+      size="small"
+      style={{ width: 90 }}
+    />
+  )
+}
